e9ec64bff271e64b32d17db2fe4421353bb9ccf5
Curtis Ullerich <curtisullerich@gmail.com>
2025-02-14 21:18:53 -0800

    Add script to publish to ulleri.ch

---
+++ sync.sh
@@ -0,0 +1,58 @@
+# check if we're in the right directory
+DIR=$(basename $(pwd))
+ULLERICH=$(readlink -f ../ulleri.ch)
+WEDDING=$(readlink -f .)
+if [[ $DIR != wedding ]]; then
+  echo "it doesn't look like you're in the root of the wedding repository"
+  exit
+fi
+if [ ! -d $ULLERICH ]; then
+  echo "ulleri.ch repository is expected to be a sibling of wedding repository"
+  exit
+fi
+echo "switching to ../ulleri.ch"
+cd $ULLERICH
+echo "current directory is $(pwd)"
+if [[ `git status --porcelain` ]]; then
+  echo "ulleri.ch has local changes"
+  exit
+fi
+echo "pulling ulleri.ch"
+git pull
+echo "switching to ../wedding and running jekyll build"
+cd $WEDDING
+bundle exec jekyll build
+if [[ ! $? ]]; then
+  echo "jekyll build had issues"
+  exit
+fi
+read -p "continue by deleting and replacing $ULLERICH/wedding?" -n 1 -r
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+  echo "exiting"
+  exit
+fi
+echo "continuing"
+rm -r $ULLERICH/wedding
+echo "copying"
+cp -r _site/wedding $ULLERICH/
+cd $ULLERICH
+git commit -a -m "Published latest changes of /wedding"
+read -p "continue by pushing ulleri.ch to github?" -n 1 -r
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+  echo "exiting"
+  exit
+fi
+echo "pushing"
+git push
+echo "done"
+
+# check if ulleri.ch exists as a sibling
+# check if ulleri.ch has pulled
+# check if ulleri.ch has any changes
+# bundle exec jekyll build
+# rm -r $ULLERICH/wedding
+# cp -r _site/wedding $ULLERICH/
+
+
